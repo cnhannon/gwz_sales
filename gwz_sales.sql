8e34e5f6e1c5a6e418d31ee5aae69c3c3a4d42cf
Chris Hannon <chris.n.hannon@gmail.com>
2024-09-09 14:07:44 +0100

    added rounding to turnover, and purchase_cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,3 +1,4 @@
+# SQL script for github
 SELECT
     date_date,
     ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost,
@@ -5,3 +6,4 @@ SELECT
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date;
+
