8a9cb1f0ae81956b7cfa1278f0815c4b8f9640a9
Chris Hannon <chris.n.hannon@gmail.com>
2024-09-09 13:18:24 +0100

    add purchase_cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,5 +1,7 @@
-SELECT date_date, ROUND(SUM(turnover), 2) AS daily_turnover
+SELECT
+    date_date,
+    ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost,
+    ROUND(SUM(turnover), 2) AS daily_turnover
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date;
-# testing
